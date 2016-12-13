df3d2b09ef316962d1cd07ae9726964ef0e9d745
Marc <dcasts1@gmail.com>
2016-12-13 15:36:21 -0500

    12.13.16 Update to Start

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,6 +1,28 @@
+import controller.ChatListener;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
 public class Start {
 
     public static void main(String[] args) {
 
+        JDA jda;
+
+        try {
+            jda = new JDABuilder(AccountType.BOT)
+                    .addListener(new ChatListener()).setToken(//add token here)
+                    .buildBlocking();
+            jda.setAutoReconnect(true);
+        } catch (LoginException e) {
+            System.err.println("Authentication Failed...");
+        } catch (InterruptedException e) {
+            System.err.println("Initialization Terminated...");
+        } catch (RateLimitedException e) {
+            System.err.println("Too many Login requests.");
+        }
     }
 }
