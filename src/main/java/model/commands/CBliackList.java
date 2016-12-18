2e421b18296a5e04e9ccfa03c81efc8ab64ab674
Marc <dcasts1@gmail.com>
2016-12-17 20:07:55 -0500

    12.17.16 CBlackList

---
+++ src/main/java/model/commands/CBliackList.java
@@ -0,0 +1,13 @@
+package model.commands;
+
+import model.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class CBliackList extends Command {
+
+
+    @Override
+    public void execute(MessageReceivedEvent event) {
+
+    }
+}
